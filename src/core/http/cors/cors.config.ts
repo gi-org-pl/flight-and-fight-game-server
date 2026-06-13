0b10103d7a9305c81d41beabef4779a238c5c03b
Oskar Barcz <oskar@barcz.me>
2026-06-13 02:20:49 +0200

    Fix prod setup for typeorm

--- src/core/http/cors/cors.config.ts
+++ src/core/http/cors/cors.config.ts
@@ -3,7 +3,9 @@ import { isProduction } from '../../consts';
 
 export function configureCors(app: INestApplication): void {
   app.enableCors({
-    origin: isProduction ? 'https://example.com' : 'http://localhost:5173',
+    origin: isProduction
+      ? 'https://api-faf.gi.org.pl/'
+      : 'http://localhost:5173',
     credentials: true, // allow cookies / Authorization headers
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
