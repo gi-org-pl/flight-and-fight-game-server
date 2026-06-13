ca6692cb860033446d60f7a00c9851f54dc4ca69
Oskar Barcz <oskar@barcz.me>
2026-06-13 02:25:55 +0200

    Fix prod setup for typeorm and set CORS headers (#3)

--- src/core/database/data-source.ts
+++ src/core/database/data-source.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata';
+import { join } from 'path';
 import { DataSource } from 'typeorm';
 import { entities } from './entities';
+
+const migrationsGlob = __filename.endsWith('.js') ? '*.js' : '*.ts';
 
 export const dataSourceOptions = {
   type: 'postgres' as const,
@@ -11,6 +14,6 @@ export const dataSourceOptions = {
 
 export default new DataSource({
   ...dataSourceOptions,
-  migrations: ['src/core/database/migration/*.ts'],
+  migrations: [join(__dirname, 'migration', migrationsGlob)],
   migrationsTableName: 'migrations',
 });
