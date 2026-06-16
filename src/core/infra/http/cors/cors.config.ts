a90a579cf98aa58ada3a3c471c91cb70efbe1903
Oskar Barcz <oskar.barcz@contractor.atolls.com>
2026-06-16 12:21:35 +0200

    Fix CORS for prod deployment

--- src/core/infra/http/cors/cors.config.ts
+++ src/core/infra/http/cors/cors.config.ts
@@ -2,7 +2,7 @@ import { INestApplication } from '@nestjs/common';
 
 export function configureCors(app: INestApplication): void {
   app.enableCors({
-    origin: ['https://faf.gi.org.pl/', 'http://localhost:5173'],
+    origin: ['https://faf.gi.org.pl', 'http://localhost:5173'],
     credentials: true,
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
